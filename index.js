00f60e8263dcd948783bdff3d1b100d414f41dc8
sammun1926 <munorusam10@gmail.com>
2023-03-19 05:15:04 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+const receivesAFunction = (spy) => {
+    spy();
+  };
+  
+  const returnsANamedFunction = () => {
+    return function receivesAFunction(name) {};
+  };
+  
+  function returnsAnAnonymousFunction() {
+    return function (name) {
+     
+    };
+  }
+ 
